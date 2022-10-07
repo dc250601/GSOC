e068c6277f6527fd187e866a75f935f5d8efa921
dc250601 <92249586+dc250601@users.noreply.github.com>
2022-10-07 19:44:25 +0530

    cleanup

--- Data_set_refined_zero_suppress/data_maker.py
+++
@@ -1,8 +0,0 @@
-from runner1 import runner
-from british import curzon
-print("Making data for test 1, 50,200")
-t = [1e-3, 3.2e-3, 1e-2, 3.2e-2, 1e-1, 3.2e-1]
-for threshold in t:
-    runner(threshold=threshold)
-    curzon(threshold)
-    print("curzon completed!")
